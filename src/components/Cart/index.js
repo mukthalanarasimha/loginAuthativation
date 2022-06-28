be3607d9dcd609ed2eda8b42cb899fded80ced6e
mukthalanarasimha <mudirajnarasimha64@gmail.com>
2022-06-28 11:18:54 +0000

    loginformAuthetication

---
+++ src/components/Cart/index.js
@@ -0,0 +1,15 @@
+import Header from '../Header'
+import './index.css'
+
+const Cart = () => (
+  <div>
+    <Header />
+    <div className="cart_container">
+      <img
+        src="https://assets.ccbp.in/frontend/react-js/nxt-trendz-cart-img.png"
+        alt="cart"
+      />
+    </div>
+  </div>
+)
+export default Cart
---
+++ src/components/Products/index.js
@@ -0,0 +1,16 @@
+import Header from '../Header'
+import './index.css'
+
+const Products = () => (
+  <div>
+    <Header />
+    <div className="product_container">
+      <img
+        src="https://assets.ccbp.in/frontend/react-js/nxt-trendz-products-img.png"
+        alt="products"
+      />
+      <h1>Products</h1>
+    </div>
+  </div>
+)
+export default Products
